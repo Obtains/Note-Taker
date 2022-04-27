c8568a7078b84c47c950937ef46926858a8dd7ae
Obtains <97128583+Obtains@users.noreply.github.com>
2022-04-27 17:08:52 -0400

    added routes folder to seperate api/html routes

--- Note-Taker/server.js
+++ Note-Taker/server.js
@@ -1,6 +1,5 @@
 // Dependencies
 const fs = require("fs");
-const userDataBase = require("./db/db.json");
 const path = require("path");
 const express = require("express");
 var PORT = process.env.PORT || 3001;
@@ -10,16 +9,12 @@ var app = express();
 app.use(express.static("public"));
 
 // Data Parsing (is required for API calls)
-app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-app.get("/", function (req, res){
-    res.sendFile(path.join(__dirname, "/public/index.html"));    
-});
-
-app.get("/notes", function (req, res){
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
+// Router Require Files
+require("./routes/apiRoutes");
+require("./routes/htmlRoutes");
 
 // Express Listening
 app.listen(PORT, () => {
